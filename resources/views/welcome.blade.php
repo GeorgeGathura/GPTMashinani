179206a80c1bc1955710af66a4040242b6f952c0
George <gkwambata@gmail.com>
2023-09-01 08:47:02 +0000

    updated content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,24 +33,23 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8 grid grid-cols-1 lg:grid-cols-2">
                 <div class="flex justify-center">
-                    <img src="{{asset('images/Logo.webp')}}" class="w-24" alt="Chat Mtaani"/>
+                    <img src="{{asset('images/Logo.webp')}}" class="w-12" alt="Chat Mtaani"/>
                 </div>
 
                 <div class="">
 
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                            <div class="flex gap-x-10 items-center align-middle">
+
                             <div>
-                                <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-red-500">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-                                    </svg>
-                                </div>
-
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">What is ChatMtaani?</h2>
-
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    We are an A.I Powered platform
+                                ChatMtaani is an innovative solution designed to bridge the digital divide by making the power of artificial intelligence accessible to farmers in rural and underserved communities.</p>
+                                <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">Utilizing a simple shortcode 23348, this service allows users to interact with ChatGPT—a highly sophisticated AI chatbot—via SMS on both feature phones and smartphones. This enables farmers to receive real-time, actionable advice on a wide range of topics, from best farming practices to market trends, all without the need for internet access or advanced technological know-how.</p>
+                                <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">By democratizing access to cutting-edge AI technology, ChatMtaani aims to empower farmers to make data-driven decisions, thereby increasing their productivity and improving their livelihoods.
                                 </p>
+
+                            </div>
                             </div>
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
