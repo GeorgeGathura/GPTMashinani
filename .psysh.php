40c4edc56f35d9148490a5da051ef260288a4ad5
George K <gkwambata@gmail.com>
2023-09-17 11:34:59 +0000

    config fix

---
+++ .psysh.php
@@ -0,0 +1,7 @@
+// Temporary fix for issue #34 in laravel tinker where psysh
+// tries to run in `/run/user/<uid>/psysh`.
+//
+// This overrides that behaviour to `.psysh/` inside the project
+return [
+	'runtimeDir'	=> './.psysh',
+];
