64982e51995cffee1b0fe92afa71ee9a0005c17d
George K <gkwambata@gmail.com>
2023-09-21 05:12:19 +0000

    updated github yml

--- .psysh.php
+++
@@ -1,7 +0,0 @@
-// Temporary fix for issue #34 in laravel tinker where psysh
-// tries to run in `/run/user/<uid>/psysh`.
-//
-// This overrides that behaviour to `.psysh/` inside the project
-return [
-	'runtimeDir'	=> './.psysh',
-];
